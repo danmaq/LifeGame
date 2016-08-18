572a5d6c7f1d25be8ea5b65ed9ce937d921c61b4
Shuhei Nomura <info@danmaq.com>
2016-08-18 22:13:28 +0900

    プロジェクト初期設定。」

---
+++ Lifegame/LG.Core.Test/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace LG.Core.Test.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// アセンブリに関する一般情報は、以下の属性セットによって
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[<assembly: AssemblyTitle("LG.Core.Test")>]
+[<assembly: AssemblyDescription("xUnit test module for LG.Core")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("danmaq / DAYS PRODUCTION")>]
+[<assembly: AssemblyProduct("LG.Core.Test")>]
+[<assembly: AssemblyCopyright("Copyright © 2016 danmaq / DAYS PRODUCTION")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// ComVisible を false に設定すると、COM コンポーネントがこのアセンブリ内のその型を認識
+// できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[<assembly: ComVisible(false)>]
+
+// このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります
+[<assembly: Guid("06b2751b-e5a6-452a-8603-e911ec15d10e")>]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されます。:
+// 
+//       メジャー バージョン
+//       マイナー バージョン 
+//       ビルド番号
+//       リビジョン
+// 
+// すべての値を指定するか、下に示すように '*' を使用してビルドおよびリビジョン番号を
+// 既定値にすることができます。:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Lifegame/LG.Core/AssemblyInfo.fs
@@ -0,0 +1,35 @@
+﻿namespace LG.Core.AssemblyInfo
+
+open System.Resources
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// アセンブリに関する一般情報は、以下の属性セットによって
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[<assembly: AssemblyTitle("LG.Core")>]
+[<assembly: AssemblyDescription("Lifegame core library")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("danmaq / DAYS PRODUCTION")>]
+[<assembly: AssemblyProduct("LG.Core")>]
+[<assembly: AssemblyCopyright("Copyright © 2016 danmaq / DAYS PRODUCTION")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+[<assembly: NeutralResourcesLanguage("ja")>]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されます。:
+// 
+//       メジャー バージョン
+//       マイナー バージョン 
+//       ビルド番号
+//       リビジョン
+// 
+// すべての値を指定するか、下に示すように '*' を使用してビルドおよびリビジョン番号を
+// 既定値にすることができます。:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
