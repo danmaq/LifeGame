af874c480a97e9c8973825f834903cc9d0f0b46d
Shuhei Nomura <info@danmaq.com>
2016-08-26 09:15:10 +0900

    セル情報追加。セル一覧実装中

---
+++ Lifegame/LG.Core/Cells.fs
@@ -0,0 +1,31 @@
+﻿module Cells
+
+///<summary>セルの状態に対する、次の生存状態。</summary>
+type Cell =
+    {
+        live: bool
+        neighbors: Cell[]
+    }
+    ///<summary>セルの状態に対する、次の生存状態。</summary>
+    static member next cell =
+        let count =
+            cell.neighbors
+                |> Array.toSeq
+                |> Seq.filter (fun c -> c.live)
+                |> Seq.length
+        cell.live && count = 2 || count = 3
+
+let create (width, height) =
+    let p2i x y = x + y * width
+    let i2p i = (i % width, i / width)
+    let neighbors =
+        let src = [| -1 .. 1 |] |> Array.toSeq
+        src
+            |> Seq.map (fun y -> src |> Seq.map (fun x -> (x, y)))
+            |> Seq.concat
+            |> Seq.toArray
+    let cells =
+        { live = false; neighbors = [||] }
+            |> Array.create (width * height)
+    cells |> Array.mapi (fun i c -> ())
+    ()
