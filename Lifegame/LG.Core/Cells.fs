34e5e62229f7921ddb6baed2d5b9b2c9fb29c0a1
Shuhei Nomura <info@danmaq.com>
2016-08-28 23:11:47 +0900

    セル一覧を生成する関数を作成。

--- Lifegame/LG.Core/Cells.fs
+++ Lifegame/LG.Core/Cells.fs
@@ -1,10 +1,12 @@
 ﻿module Cells
 
-///<summary>セルの状態に対する、次の生存状態。</summary>
+///<summary>セルの状態。</summary>
 type Cell =
     {
+        ///<summary>生存しているかどうか。</summary>
         live: bool
-        neighbors: Cell[]
+        ///<summary>隣接セルへの参照。</summary>
+        neighbors: Cell []
     }
     ///<summary>セルの状態に対する、次の生存状態。</summary>
     static member next cell =
@@ -15,17 +17,48 @@ type Cell =
                 |> Seq.length
         cell.live && count = 2 || count = 3
 
-let create (width, height) =
-    let p2i x y = x + y * width
-    let i2p i = (i % width, i / width)
-    let neighbors =
-        let src = [| -1 .. 1 |] |> Array.toSeq
-        src
-            |> Seq.map (fun y -> src |> Seq.map (fun x -> (x, y)))
-            |> Seq.concat
-            |> Seq.toArray
-    let cells =
-        { live = false; neighbors = [||] }
-            |> Array.create (width * height)
-    cells |> Array.mapi (fun i c -> ())
-    ()
+///<summary>隣接セルのオフセット。</summary>
+let private neighborsOffset =
+    let src = [| -1 .. 1 |] |> Array.toSeq
+    src
+        |> Seq.map (fun y -> src |> Seq.map (fun x -> (x, y)))
+        |> Seq.concat
+        |> Seq.filter (fun xy -> xy <> (0, 0))
+        |> Seq.toArray
+
+///<summary>セル一覧の状態。</summary>
+type Cells =
+    {
+        ///<summary>セルの列数。</summary>
+        width: int
+        ///<summary>セルの行数。</summary>
+        height: int
+        ///<summary>セル一覧。</summary>
+        cells: Cell []
+    }
+    ///<summary>列数・X座標・Y座標に対応する、一次元インデックス。</summary>
+    static member toIndex w x y = y * w + x
+    ///<summary>列数・一次元インデックスに対応する、X座標・Y座標。</summary>
+    static member toPos w i = (i % w, i / w)
+    ///<summary>セル情報一覧とセル情報に対応する座標。</summary>
+    static member pos cells cell =
+        match cells.cells |> Array.tryFindIndex (fun c -> c = cell) with
+            | Some c -> c |> Cells.toPos cells.width |> Some
+            | None -> None
+    ///<summary>列数・行数に対応するセル情報一覧。</summary>
+    static member create (width, height) =
+        let cells =
+            { live = false; neighbors = [||] }
+                |> Array.create (width * height)
+        let neighborsIndex i =
+            let (bx, by) = i |> Cells.toPos width
+            neighborsOffset
+                |> Array.toSeq
+                |> Seq.map (fun (x, y) -> Cells.toIndex width x y)
+                |> Seq.map (fun i -> cells.[i])
+                |> Seq.toArray
+        {
+            width = width
+            height = height
+            cells = cells |> Array.mapi (fun i c -> { c with neighbors = i |> neighborsIndex })
+        }
